75543d573c7642e0a05b8c5429382cb082a3381d
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-06-17 16:10:09 +0100

    feat: add multiple file scanning

--- polyglot_api/index.py
+++ polyglot_api/index.py
@@ -29,16 +29,18 @@ def index():
 
 @app.route('/', methods=['POST'])
 def post_index():
-    if 'file' not in request.files:
+    if 'file[]' not in request.files:
         return render_template('index.html'), 400
-    file = request.files['file']
-    if file.filename == '':
-        return render_template('index.html'), 400
-    filename = secure_filename(file.filename)
-    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(path)
-    results = Analysis(file.filename, path)
-    os.remove(path)
+    files = request.files.getlist('file[]')
+    results = []
+    for file in files:
+        if file.filename == '':
+            return render_template('index.html'), 400
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+        results.append(Analysis(file.filename, path))
+        os.remove(path)
     return render_template('index.html', results=results)
 
 
