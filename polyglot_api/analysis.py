3cc5b7cc554b6b055735e4150f4a5592aa8d77ea
H.L.J.Laloge <hljl2@kent.ac.uk>
2018-06-17 15:07:16 +0100

    feat: improve result display and add TrID scan

--- polyglot_api/analysis.py
+++ polyglot_api/analysis.py
@@ -1,15 +1,19 @@
+import logging
+import re
+import subprocess
 import time
 
 from polyglot_detector import PolyglotLevel, scan
 from polyglot_detector.magic import magic_scan
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
 
 class AnalysisEntry:
-    def __init__(self, type: str, level: PolyglotLevel, found_by_magic: bool, was_embedded: bool=False):
-        self.type = type
+    def __init__(self, ext: str, level: PolyglotLevel):
+        self.ext = ext
         self.level = level
-        self.found_by_magic = found_by_magic
-        self.was_embedded = was_embedded
 
 
 class Analysis:
@@ -21,14 +25,15 @@ class Analysis:
         self.elapsed_time = time.time() - start_time
         self.results = []
 
-        magic_scan_results = magic_scan(path)
+        self.magic_scan_results = magic_scan(path)
+        self.trid_scan_results = trid_scan(path)
 
         for type, level in self.scan_results.items():
             level_without_embedded = level & ~PolyglotLevel.EMBED
             for embedded_type in level.embedded:
-                self.results.append(AnalysisEntry(embedded_type, level_without_embedded, embedded_type in magic_scan_results))
-            self.results.append(AnalysisEntry(type, level_without_embedded, type in magic_scan_results))
-        self.results.sort(key=lambda entry: entry.type)
+                self.results.append(AnalysisEntry(embedded_type, level_without_embedded))
+            self.results.append(AnalysisEntry(type, level_without_embedded))
+        self.results.sort(key=lambda entry: entry.ext)
 
     def __iter__(self):
         return self.results.__iter__()
@@ -40,3 +45,21 @@ class Analysis:
     @property
     def is_dangerous(self):
         return len(self.results) > 1
+
+
+TRID_RESULT_RE = re.compile('^\s*(\d+\.\d+)% \(\.([^)]+)\) ')
+
+
+def trid_scan(path: str) -> {str: float}:
+    output = subprocess.check_output(['/opt/trid/trid', '-n:5', path], env={'LC_ALL': 'C.UTF-8'})
+    output = output.decode('ascii')
+    result_lines = output.split('\n')[6:]
+    results = {}
+    for line in result_lines:
+        matches = TRID_RESULT_RE.match(line)
+        if not matches:
+            break
+        percentage, ext = float(matches[1]), matches[2].lower()
+        results[ext] = results.get(ext, 0.0) + percentage
+    logger.debug('TRiD results: %s' % results)
+    return results
--- polyglot_api/index.py
+++ polyglot_api/index.py
@@ -30,13 +30,16 @@ def index():
 @app.route('/', methods=['POST'])
 def post_index():
     if 'file' not in request.files:
-        return 400
+        return render_template('index.html'), 400
     file = request.files['file']
     if file.filename == '':
-        return 400
+        return render_template('index.html'), 400
     filename = secure_filename(file.filename)
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(path)
     results = Analysis(file.filename, path)
     os.remove(path)
     return render_template('index.html', results=results)
+
+
+app.jinja_env.globals['repository_url'] = 'https://git.cs.kent.ac.uk/hljl2/polyglot-detector'
